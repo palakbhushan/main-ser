f9acbc9f69c6fdeccb06d8b757950796fc096ae1
palakbhushan <palakbosw@gmail.com>
2022-08-23 23:20:36 +0530

    added code ons fsss

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ http.createServer(function (req, res) {
   res.write('JMD'); //write a response to the client
   res.end(); //end the response
 }).listen(5005);
-console.log('Running at port 5005 is running..')
+console.log('Running at port 5005 is running...')
 
 
