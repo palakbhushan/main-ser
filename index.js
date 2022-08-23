7f3d0714704fd735388d45a2b1ef25c9447c9669
palakbhushan <palakbosw@gmail.com>
2022-08-23 16:56:09 +0530

    server added

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-hi
+var http = require('http');
+
+//create a server object:
+http.createServer(function (req, res) {
+  res.write('Hello universe!'); //write a response to the client
+  res.end(); //end the response
+}).listen(5005);
