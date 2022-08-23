15b7ffb74482e2e5780e7787c2e69f90128f9a13
palakbhushan <palakbosw@gmail.com>
2022-08-23 22:11:32 +0530

    added code ons fsss

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 //create a server object:
 http.createServer(function (req, res) {
-  res.write('he ha hu'); //write a response to the client
+  res.write('he ha hu time'); //write a response to the client
   res.end(); //end the response
 }).listen(5005);
 console.log('Nde.js wdebb sfdexrjdfhjvedoner att port 5005 is running..')
