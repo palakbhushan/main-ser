0173456babb3c45adbced9823b62d3dd7afec4e2
palakbhushan <palakbosw@gmail.com>
2022-08-23 18:51:35 +0530

    added code ons f

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 //create a server object:
 http.createServer(function (req, res) {
-  res.write('Hello universe! bohiiiiiidddd'); //write a response to the client
+  res.write('Hello universe! ddd'); //write a response to the client
   res.end(); //end the response
 }).listen(5005);
 console.log('Node.js web serjdfhjver at port 5005 is running..')
